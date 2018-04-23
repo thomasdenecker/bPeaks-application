bbfb9cb0d65201691fcafb8060c7548c04d7475d
eleonore Pillot-Lucas <eleonore.pillot.lucas.25@gmail.com>
2018-04-23 16:19:42 +0200

    new PBC function

--- mybranch/PBC.R
+++ mybranch/PBC.R
@@ -6,10 +6,48 @@ data(dataPDR1)
 
 PBC=function(chrx,fichier){
   subtab=subset(fichier,V1 == chrx)
-n1= length(which(subtab[,3]==1))
-nd= length(which(subtab[,3]>=1))
-print(paste("le PBC de",chrx,"vaut",n1/nd))
-return(n1/nd)
+  n1= length(which(subtab[,3]==1))
+  nd= length(which(subtab[,3]>=1))
+  print(paste("le PBC de",chrx,"vaut",n1/nd))
+  return(n1/nd)
 }
 
 PBC(chrx = "chrIV", fichier = dataPDR1[["IPdata"]])
+
+
+# PBC deuxiemes essai
+
+rePBC=function(chrx,fichier){
+  ni=0
+  np=0
+  Fi=0
+  Fp=0
+  subtab=subset(fichier,V1 == chrx)
+  for (i in subtab){
+    if(i$V3 == 1){
+      if (Fp==T){
+        Fp=F
+        np=np+1
+      }else {Fi=T}
+    }else if(i$V3 > 1){
+      if(Fi == T){
+        Fi=F
+        ni=ni+1
+      }else {
+        Fp=T
+      } 
+    }else if(Fi==T){
+      Fi=F
+      ni=ni+1
+    }else if (Fp==T){
+      Fp=F
+      np=np+1
+      
+    }
+  }
+  print(ni/(np+ni))
+  return(ni/(np+ni))
+}
+
+a =PBC(chrx = "chrIV", fichier = dataPDR1[["IPdata"]])
+
