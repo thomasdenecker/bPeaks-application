15ca6cac0b4583b2c8620e67a456e0af7fa23011
thomasdenecker <thomas.denecker@gmail.com>
2018-06-18 15:54:08 +0200

    Add bPeaks presentation pdf

--- bPeaks_application.bat
+++
@@ -1,2 +0,0 @@
-FOR /F "tokens=*" %%g IN ('docker ps -a -q') do (docker stop %%g)
-docker run --rm -p 3838:3838 -v C:\Users\thomas\Desktop\bPeaks-application:/srv/shiny-server tdenecker/bpeaks_docker
